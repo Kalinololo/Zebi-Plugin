23f22591300ac15dd2fdc1d1b05ba61084cd9a12
Loan-Matarise <loan.matarise@etu.umontpellier.fr>
2021-05-17 19:02:55 +0200

    Ah.

---
+++ src/main/java/plugin/kits/abilities/GrandMere.java
@@ -0,0 +1,18 @@
+package plugin.kits.abilities;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import plugin.kits.KitListener;
+
+
+
+public class GrandMere extends KitListener {
+    @EventHandler
+    public void onSoupe(PlayerItemConsumeEvent e){
+        if(e.getItem().getType() == Material.MUSHROOM_SOUP && hasAbility(e.getPlayer())){
+            double miam = e.getPlayer().getHealth();
+            e.getPlayer().setHealth(miam + 4);
+        }
+    }
+}
